ac115a6a381f32694840f324b1ff328e8310dc9f
ningh <996099092@qq.com>
2019-07-15 14:46:02 +0800

    首次上传项目

---
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -0,0 +1,15 @@
+package com.pinyougou.sellergoods.service;
+
+import com.pinyougou.pojo.TbBrand;
+
+import java.util.List;
+
+/**
+ * 品牌接口
+ * @author ningh
+ */
+public interface BrandService {
+
+    public List<TbBrand> findAll();
+
+}
